62a53e218cbb976849b07cbca7e32d5c65840307
Fox Piacenti <fox@vulpinity.com>
2021-11-19 11:43:38 -0600

    Initial commit

---
+++ src/example/ExamplePage.jsx
@@ -0,0 +1,10 @@
+export default function ExamplePage() {
+  return (
+    <main>
+      <div className="container-fluid">
+        <h1>Example Page</h1>
+        <p>Hello world!</p>
+      </div>
+    </main>
+  );
+}
